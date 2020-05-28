05e8dd233e34ffa620211c2ce877d9e480c55af3
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-05-28 20:59:16 +0100

    configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request,response) =>{
-    return response.send('Hello Word');
+    return response.send('Hello Word 2');
 })
 
 
